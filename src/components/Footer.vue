c514c6e7ee90fafa559743ba2bee7f8fc741c16b
vanezamaediarios <vanezamaediarios@gmail.com>
2026-05-21 16:42:06 +0800

    removed bg-light in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
 	<!-- Footer Start -->
-		<footer id="footer" class="bg-light justify-content-center align-items-center column d-flex flex-column py-3">
+		<footer id="footer" class=" justify-content-center align-items-center column d-flex flex-column py-3">
 			<div>Vaneza Diarios</div>
 			<div>Full Stack Web Developer</div>
 			<div>&copy; All Rights Reserved 2026</div>
